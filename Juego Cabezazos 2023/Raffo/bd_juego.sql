215c0f609998531bb71219f07984aca9d144fcff
RaffoAgustin <113942433+RaffoAgustin@users.noreply.github.com>
2023-11-10 14:46:51 -0300

    Add files via upload

---
+++ Juego Cabezazos 2023/Raffo/bd_juego.sql
@@ -0,0 +1,45 @@
+--
+-- Base de datos: `juego`
+--
+CREATE DATABASE IF NOT EXISTS `juego` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `juego`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugador`
+--
+
+CREATE TABLE `jugador` (
+  `id` int(11) NOT NULL,
+  `ganadas` int(11) NOT NULL,
+  `perdidas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `jugador`
+--
+
+INSERT INTO `jugador` (`id`, `ganadas`, `perdidas`) VALUES
+(1, 14, 25);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
